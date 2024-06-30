938f70d7fb99d7f3511530168a3cc9fca8a54696
DGHaZe <34streets@gmail.com>
2024-06-30 11:06:03 +0300

    edit file

--- 16_9.cpp
+++ 16_9.cpp
@@ -4,18 +4,41 @@ using namespace std;
 
 int main()
 {
-    char ch;
+    /*char ch;
     cout << "enter a phrase: ";
     while (cin.get(ch))
     {
+
         if (ch == '!')
             cin.putback('$');
         else
             cout << ch;
         while (cin.peek() == '#')
             cin.ignore(1, '#');
-    }
 
+    }*/
+
+    char text[256];
+    int sizeMas = sizeof(text);
+    cout << "enter a phrase: ";
+    cin >> text;
+    
+    for(int i = 0; i < sizeMas; i++)
+    {
+        if (text[i] == '!')
+        {
+            char lit = '$';
+            text[i] = lit;
+        }
+
+        if (text[i] == '#')
+        {
+            //char lit = '$';
+            text[i] = text[i -1];
+        }
+    }
+    cout << text;
+    
     return 0;
 }
 ////
